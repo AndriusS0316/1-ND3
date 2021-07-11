f7a6bf4fc994695ea0a76ba40e36899e6ae2f426
Andrius <andriuss0316@gmail.com>
2021-07-11 14:27:18 +0300

    20210711

---
+++ main.js
@@ -0,0 +1,12 @@
+// trecia uzduotis 
+
+const h1ND3 = document.querySelector('h1');
+h1ND3.classList.add('simba');
+
+const allIngredientsND3 = document.querySelectorAll('li');
+
+for (let i = 0; i < allIngredientsND3.length; i++) {
+    allIngredientsND3[i].classList.add('darzove');
+  }
+
+  
