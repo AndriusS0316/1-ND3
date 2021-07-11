98a385943ee4ddf33924c21b2e0c93c08f2e3843
Andrius <andriuss0316@gmail.com>
2021-07-11 15:56:45 +0300

    20210711

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ h1ND3.classList.add('simba');
 const allIngredientsND3 = document.querySelectorAll('li');
 
 for (let i = 0; i < allIngredientsND3.length; i++) {
-    allIngredientsND3[i].classList.add('darzove');
-  }
+  allIngredientsND3[i].classList.add('darzove');
+}
 
   
